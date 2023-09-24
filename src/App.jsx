e38b20adf0e6a6750f7262a50712e7a9384c0750
thakiddx <xmalone0920@gmail.com>
2023-09-24 16:25:12 -0700

    addNames

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Component1 from './components/Component1/counter';
 import './App.css';
 import SimpleInput from './components/Component2/simpleInput';
 import RandomName from './components/Component3/randomName';
+import AddNames from './components/Component4/addNames';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Component1 />
       <SimpleInput />
       <RandomName />
+      <AddNames />
       {/* Other components or content */}
     </div>
   );
